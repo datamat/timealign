cb505cea0b5a3053554f7f4cbd132f36a26db64f
datamat <matthias.haeni@wsl.ch>
2015-08-10 16:00:19 +0200

    changed back some things...

--- example.R
+++ example.R
@@ -1,13 +1,21 @@
 # Matthias Haeni
+# 2011-2015, extract of treegro
+# last change August 9, 2015
 
-require(RCurl)
-source_https <- function(url, ...) {
-  sapply(c(url, ...), function(u) {
-    eval(parse(text = getURL(u,
-                             followlocation = TRUE,
-                             cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))),
-         envir = .GlobalEnv)
-  })
-}
+library(chron)
+source("https://raw.githubusercontent.com/datamat/timealign/master/timealign.R")
+x <- "https://raw.githubusercontent.com/datamat/timealign/master/exampledata.RData"
+load(url(x))
 
-source_https("https://raw.githubusercontent.com/datamat/timealign/master/timealign.R")
+head(df)
+tail(df)
+
+# First column has to be a POSIXct-class and the name has to be "ts"
+# the second column has to be "val"
+foo <- tsalign(df,reso=10)
+
+head(foo)
+tail(foo)
+
+# See that foo has now a perfect 10 minute time stamp, df didn't
+# Any questions? Please let me know at matthias.haeni-AT-wsl.ch
